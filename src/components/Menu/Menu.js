8cf6b953f9a2c91a067ecdc9460eb188d21c7ccf
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-20 09:58:02 -0700

    Update styling for Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,11 +5,11 @@ const Menu = () => {
   return (
     <header className="menu">
       <h1>What's <span className="new">New?</span></h1>
-      <h2>Local News</h2>
-      <h2>Technology</h2>
-      <h2>Entertainment</h2>
-      <h2>Science</h2>
-      <h2>Health</h2>
+      <h2 className="topic">Local News</h2>
+      <h2 className="topic">Technology</h2>
+      <h2 className="topic">Entertainment</h2>
+      <h2 className="topic">Science</h2>
+      <h2 className="topic">Health</h2>
     </header>
   )
 }
