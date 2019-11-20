0fb0ceb4d21b6aebb74361f3caf7de9429e16df4
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-19 17:37:51 -0700

    Create NewsContainer component to hold the search bar and all the articles

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import './NewsContainer.css'
+import './NewsContainer.css';
+import NewsArticle from '../NewsArticle/NewsArticle';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({ selected }) => {
+  console.log(selected)
+  const newsCards = selected.map(article => {
+    return (
+      <NewsArticle 
+        key={article.id}   
+        id={article.id}
+        title={article.headline}
+        description={article.description}
+        url={article.url}
+        img={article.img}
+      />
+    )
+  })
+
+
+  return (
+    <main>
+      <section className="search">
+        <input className="search-input" type="text" placeholder="Search for news articles here."></input>
+        <button className="search-btn" type="button">Search Now</button>
+      </section>
+      <section className="news-container">
+        {newsCards}
+      </section>
+    </main>
+  )
+}
 
 export default NewsContainer;
