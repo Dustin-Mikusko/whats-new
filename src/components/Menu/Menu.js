4825fc7051f7ea3f5e82481036223dac457f8cf5
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-19 17:34:35 -0700

    Create Menu component for different topics

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+  return (
+    <header className="menu">
+      <h1>What's <span className="new">New?</span></h1>
+      <h2>Local News</h2>
+      <h2>Technology</h2>
+      <h2>Entertainment</h2>
+      <h2>Science</h2>
+      <h2>Health</h2>
+    </header>
+  )
+}
 
 export default Menu;
