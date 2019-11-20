ab189a2ee4904d7692b4426d58799758fb704c47
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-19 17:35:38 -0700

    Create NewsArticle component to build and style each news card

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = ({ id, title, description, img, url}) => {
+  return (
+    <article className="article">
+      <img src={img}></img>
+      <div className="article-content">
+        <h2 className="article-title">{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="link">
+        <a href={url}>Link To Article</a>
+        <img src="../images/arrow.png"></img>
+      </div>
+    </article>
+  )
+}
 
 export default NewsArticle;
