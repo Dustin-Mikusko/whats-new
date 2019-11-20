aeadeab7bac221184364cdc1992746e7106e2081
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-19 17:33:20 -0700

    Update App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import Menu from '../Menu/Menu';
+import NewsContainer from '../NewsContainer/NewsContainer';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      selected: local
     }
   }
 
   render () {
+    console.log(this.state.selected)
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+      <Menu />
+      <NewsContainer selected={this.state.selected}/>
       </div>
     );
   }
