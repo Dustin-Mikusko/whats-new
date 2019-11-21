33be0911be4ac9ce247781419ee3fd45cf5b2f91
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-20 19:40:54 -0700

    Add background border functionality for selected topic

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
 import './Menu.css';
-import { GoRocket, GoLocation } from 'react-icons/go';
+import { GoLocation } from 'react-icons/go';
 import { GiGears, GiTicket } from 'react-icons/gi';
 import { FaHeartbeat } from 'react-icons/fa';
+import { GiMicroscope } from 'react-icons/gi';
 
 class Menu extends Component {
   constructor(props) {
     super(props);
+    this.topics = null;
+  }
+
+  highlightSelection = (event) => {
+    this.topics.forEach(topic => {
+      topic.classList.remove('active');
+    });
+    event.target.classList.add('active');
   }
 
   submitSelection = (event) => {
+    this.topics = document.querySelectorAll('.topic');
     const { changeNews } = this.props;
     const selection = event.target.dataset.id;
-    console.log(selection);
+    this.highlightSelection(event);
     changeNews(selection);
   }
+
+ 
 
   render() { 
     return (
     <header className="menu">
       <h1>What's <span className="new">New?</span></h1>
-      <h2 data-id="local" className="topic" onClick={(event) => this.submitSelection(event)}><GoLocation /> Local News</h2>
+      <h2 data-id="local" className="topic active" onClick={(event) => this.submitSelection(event)}><GoLocation /> Local News</h2>
       <h2 data-id="technology" className="topic" onClick={(event) => this.submitSelection(event)}><GiGears /> Technology</h2>
       <h2 data-id="entertainment" className="topic" onClick={(event) => this.submitSelection(event)}><GiTicket /> Entertainment</h2>
-      <h2 data-id="science" className="topic" onClick={(event) => this.submitSelection(event)}><GoRocket /> Science</h2>
+      <h2 data-id="science" className="topic" onClick={(event) => this.submitSelection(event)}><GiMicroscope /> Science</h2>
       <h2 data-id="health" className="topic" onClick={(event) => this.submitSelection(event)}><FaHeartbeat /> Health</h2>
     </header>
     )
