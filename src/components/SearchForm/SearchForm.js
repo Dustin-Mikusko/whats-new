36d5f5fbe1c75913ba3192f1aa154447d375f49d
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-20 19:08:23 -0700

    Move JSX and CSS styling to this component for mor granular code

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <form className="search">
+        <input className="search-input" type="text" placeholder="Search for news articles here."></input>
+        <button className="search-btn" type="button">Search Now</button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
