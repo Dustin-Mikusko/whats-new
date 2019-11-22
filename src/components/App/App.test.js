98b2f301d03e93575e846ba13bf14c7512022969
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-22 16:40:57 -0700

    Add test for getFilteredNews and update Snapshot

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -15,11 +15,26 @@ describe('App', () => {
   });
 
   it('should update state with selected news from Menu component', () => {
-
     wrapper.instance().changeNews('science');
 
     expect(wrapper.state('news')).toEqual(science);
+  });
 
+  it('should update state with the filtered news results based on user search', () => {
+    const mockUserSearch = 'Capitol Hill';
 
+    const mockState = [
+      {
+      id: 5,
+      headline: 'Motorcyclist killed in crash near Capitol Hill in Denver',
+      img: 'https://localtvkdvr.files.wordpress.com/2019/09/fatal-motorcycle.jpeg?quality=85&strip=all&w=800&h=450&crop=1',
+      description: 'A motorcyclist was killed in a crash near East 17th Avenue and Park Avenue in Denver early Friday morning.',
+      url: 'https://kdvr.com/2019/09/27/motorcyclist-killed-in-crash-near-capitol-hill-in-denver/'
+    }
+  ];
+
+    wrapper.instance().getFilteredNews(mockUserSearch);
+
+    expect(wrapper.state('news')).toEqual(mockState)
   })
 })
